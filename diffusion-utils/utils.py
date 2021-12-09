1c65e78f40d311129beb15c7be4db8e3c255d6b4
theseriousadult <j@doodlebot.ai>
2021-12-09 21:40:52 +0000

    add missing utils

--- diffusion-utils/utils.py
+++ diffusion-utils/utils.py
@@ -70,3 +70,43 @@ def mk_full(d: int, init: Union[torch.Tensor, Number]):
         return init
     else:
         return torch.full([d], init)
+
+
+@torch.no_grad()
+def ema_update(model, averaged_model, decay):
+    """Incorporates updated model parameters into an exponential moving averaged
+    version of a model. It should be called after each optimizer step."""
+    model_params = dict(model.named_parameters())
+    averaged_params = dict(averaged_model.named_parameters())
+    assert model_params.keys() == averaged_params.keys()
+
+    for name, param in model_params.items():
+        averaged_params[name].lerp_(param, 1 - decay)
+
+    model_buffers = dict(model.named_buffers())
+    averaged_buffers = dict(averaged_model.named_buffers())
+    assert model_buffers.keys() == averaged_buffers.keys()
+
+    for name, buf in model_buffers.items():
+        averaged_buffers[name].copy_(buf)
+
+
+def get_ddpm_schedule(t):
+    """Returns log SNRs for the noise schedule from the DDPM paper."""
+    return -torch.expm1(1e-4 + 10 * t**2).log()
+
+
+def get_alphas_sigmas(log_snrs):
+    """Returns the scaling factors for the clean image and for the noise, given
+    the log SNR for a timestep."""
+    alphas_squared = log_snrs.sigmoid()
+    return alphas_squared.sqrt(), (1 - alphas_squared).sqrt()
+
+
+def calculate_stats(e):
+    e_mean = e.mean()
+    e_variance = (e - e_mean).pow(2).mean()
+    e_variance_stable = max(e_variance, 1e-5)
+    e_skewness = (e - e_mean).pow(3).mean() / e_variance_stable**1.5
+    e_kurtosis = (e - e_mean).pow(4).mean() / e_variance_stable**2
+    return e_mean, e_variance, e_skewness, e_kurtosis
